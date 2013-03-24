fbe9e6dbd9d936a758b8e7a58e7c5e405ad00219
lgargarita <hilarryous@gmail.com>
2013-03-23 21:19:57 -0700

    Menu class + method to keep main clean

---
+++ src/Menu.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Menu {
+  private static int choice;
+	private static List contactlist;
+
+		
+	public static void printMenu() {
+		contactlist = new List();
+		contactlist.loadData(); //TODO: Need a way to create a save file when the program is first started up
+		Scanner console = new Scanner(System.in);
+		System.out.println("What would you like to do?" + "\n"
+				+ "1: Enter a new Contact" + "\n" + "2: Retrieve a Contact"
+				+ "\n" + "3: Print a Contact List" + "\n" + "4: Save and quit"+"\n");
+
+		do {
+			System.out.println("Please enter a number from 1-4");
+			while (!console.hasNextInt()) {
+				System.out.println("That's not a number! Please enter a number from 1-4");
+				console.next();
+			}
+			choice = console.nextInt();
+		} while (choice < 1 || choice > 4);
+
+		switch (choice) {
+		case 1:
+			System.out.println("--- Enter New Contact ---"+"\n");
+			contactlist.addContact(); 
+			break;
+		case 2:
+			int choice2;
+			System.out.println("Retrieve contacts by..." + "\n"
+					+ "1: Last Name" + "\n" + "2: E-mail address" + "\n"
+					+ "3: Zip Code");
+			do {
+				System.out.println("Please enter a number from 1-3");
+				while (!console.hasNextInt()) {
+					System.out.println("That's not a number! Please enter a number from 1-3");
+					console.next();
+				}
+				choice2 = console.nextInt();
+			} while (choice2 < 1 || choice2 > 3);
+			switch (choice2) {
+			case 1:
+				contactlist.searchByLastName();
+				break;
+			case 2:
+				contactlist.searchByEmail();
+				break;
+			case 3:
+				contactlist.searchByZip();
+				break;
+			}
+			break;
+		case 3:
+			contactlist.printContacts();
+			break;
+		case 4:
+			contactlist.saveData();
+			break;
+		default:
+			System.out.println("Invalid Response. Please try again.");
+			choice = console.nextInt();
+
+		}
+		console.close();
+	}
+
+}
